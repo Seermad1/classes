bc09af7f39bc82d9d1c3cc2d04b0b54ee93b1f69
Seermad1 <100522473+Seermad1@users.noreply.github.com>
2022-05-27 12:27:26 +0100

    Update main.py
    modified

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def change_name(name):
-    print(name)
-
-
 class Student:
     # [assignment] Skeleton class. Add your code here
     def __init__(self, name, age, tracks, score):
@@ -10,12 +6,19 @@ class Student:
         self.track = tracks
         self.score = score
         pass
+    
+    def change_name(self, name):
+        self.name = name
+        print(self.name)
 
     def change_age(self, age):
-        print(int(age))
+        self.age = age
+        print(int(self.age))
+        
     def add_track(self, track):
         self.track.append(track)
         print(self.track)
+        
     def get_score(self):
         print(float(self.score))
 
@@ -24,7 +27,7 @@ class Student:
 Bob = Student("Bob", 26, ["FE", "BE"], 20.90)
 
 # Expected methods
-change_name("Peter")
+Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
